218022542bc6baca1e5be23e63ac12abbc880ec6
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-09-28 15:40:58 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,5 @@
 # cookbook:: linux_AD
 # Recipe:: default
-# Copyright:: 2017, The Authors, All Rights Reserved.
-if node['resolver']['nameservers'].empty? ||
-   node['resolver']['nameservers'][0].empty?
-  Chef::Log.warn("The ['resolver']['nameservers'] attribute is not set.")
-  Chef::Log.warn('Exiting the recipe...')
-  return
-end
 case node['platform']
 when 'amazon'
   include_recipe 'linux_AD::linux_ad'
