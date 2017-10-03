d57b7b79d64c52e9cbae578303398ebc18fa664a
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-10-03 15:43:01 +0530

    Update ubuntu_ad.rb

--- recipes/ubuntu_ad.rb
+++ recipes/ubuntu_ad.rb
@@ -18,7 +18,19 @@ execute 'install' do
   command 'sudo apt-get install pbis-open -y'
 end
 execute 'dnsupdate' do
-  command "nameserver=node['resolver']['nameservers']"
+  command "if node['resolver']['nameservers'].empty? || node['resolver']['nameservers'][0].empty?
+  Chef::Log.warn("#{linux_AD}::#{ubuntu_ad.rb} requires that attribute ['resolver']['nameservers'] is set.")
+  Chef::Log.info("#{linux_AD}::#{ubuntu_ad.rb} exiting to prevent a potential breaking change in /etc/resolv.conf.")
+  return
+else
+  t = template '/etc/resolv.conf' do
+    source 'resolv.conf.erb'
+    owner 'root'
+    group node['root_group']
+    mode '0644'
+    # This syntax makes the resolver sub-keys available directly
+    variables node['resolver']
+  end"
 end
 execute 'domain' do
   command 'domainjoin-cli join priyanka.in Administrator pass@1234'
